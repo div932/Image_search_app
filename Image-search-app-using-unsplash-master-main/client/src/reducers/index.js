3b71dd76be08c72f1edbde1ae7c48ca76e0d4f4e
div932 <106827083+div932@users.noreply.github.com>
2023-05-25 23:07:19 +0530

    Delete Image-search-app-using-unsplash-master-main directory

--- Image-search-app-using-unsplash-master-main/client/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import imageReducer from "./imageReducer";
-import searchbarReducer from "./searchbarReducer";
-
-export default combineReducers({
-  image: imageReducer,
-  search: searchbarReducer,
-  //   error: errorReducer,
-});
